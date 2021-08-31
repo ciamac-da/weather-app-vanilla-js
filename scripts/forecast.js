91883bb83c1f02b71c4c79ec5eafa20effe09761
ciamac-da <ciamac.davoudi@gmail.com>
2021-08-31 09:36:34 +0200

    Verified by Marti4n v1.0.4 get weather API call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,18 @@
 const key = "6QKGBuSAmtzGGxw8k4Q7x7snCrqAJMbp"
 
+
+// Get weather informa
+const getWeather = async (id) => {
+
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/"
+    const query = `${id}?apikey=${key}`
+
+    const response = await fetch(base + query)
+    const data = await response.json()
+    return data[0]
+}
+
+// Get city information
 const getCity = async (city) => {
 
     const base =  "http://dataservice.accuweather.com/locations/v1/cities/search"
@@ -9,6 +22,9 @@ const getCity = async (city) => {
     const data = await response.json()
     return data[0]
 }
+
 getCity("berlin")
+.then(data => getWeather(data.Key))
 .then(data => console.log(data))
 .catch(err => console.error(err))
+
