037d8b408b9fb8823b50e4f456d31516f85de2d0
ciamac-da <ciamac.davoudi@gmail.com>
2021-08-31 12:53:42 +0200

    Verified by Marti4n v1.0.7 weather icons and images

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 const cityForm = document.querySelector("form")
 const card = document.querySelector(".card")
 const details = document.querySelector(".details")
-
+const time = document.querySelector("img.time")
+const icon = document.querySelector(".icon img")
 
 
 const updateUI = (data) => {
@@ -18,6 +19,19 @@ const updateUI = (data) => {
         <span>&deg;C</span>
     </div>
     `
+
+    // Update the night and day images
+    const iconSrc = `./assets/icons/${weather.WeatherIcon}.svg`
+    icon.setAttribute("src", iconSrc)
+
+    let timeSrc = null
+    if(weather.IsDayTime) {
+        timeSrc = "./assets/day.svg"
+    } else {
+        timeSrc = "./assets/night.svg"
+    }
+    time.setAttribute("src", timeSrc)
+
     // Remove d-none class from card and add to details
     if(card.classList.contains("d-none")) {
         card.classList.remove("d-none")
